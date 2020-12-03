e5b7d071c6bab401134ed7a413226c6f30e7cb04
Aaron Blew <aaron@blewtech.com>
2020-12-02 22:51:30 -0800

    Day 2 done!

--- day2/p2.py
+++ day2/p2.py
@@ -8,12 +8,13 @@ invalidcount = 0
 for line in input:
     entry = line.rstrip().split(' ')
 
-    counts = entry[0].split("-")
-    min_count = int(counts[0])
-    max_count = int(counts[1])
+    positions = entry[0].split("-")
+    p1 = int(positions[0]) - 1
+    p2 = int(positions[1]) - 1
     test_letter = entry[1][0]
     
-    if entry[2].count(test_letter) >= min_count and entry[2].count(test_letter) <= max_count:
+    # so an xor in python with strings is not "easy"...need to learn a more elegant way.
+    if (entry[2][p1] == test_letter and not entry[2][p2] == test_letter) or (not entry[2][p1] == test_letter and entry[2][p2] == test_letter):
         validcount += 1
     else:
         invalidcount += 1
